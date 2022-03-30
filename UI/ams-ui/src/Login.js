bbd5848c4d2b5a5521a8d4265b8ad7aabd2ec33d
sachinvm <sachin.murthy97@gmail.com>
2022-03-29 23:08:41 -0400

    Login UI changes

---
+++ UI/ams-ui/src/Login.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './Login.css';
+
+async function loginUser(username, password) {
+  var data = JSON.stringify({
+    "uname": username,
+    "password": password
+  });
+  
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+  
+  xhr.addEventListener("readystatechange", function() {
+    if(this.readyState === 4) {
+      console.log(this.responseText)
+      var response = JSON.parse(this.responseText);
+      return response.token
+    }
+  });
+  
+  xhr.open("POST", "http://localhost:8081/authentication/login");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  
+  xhr.send(data);
+}
+
+export default function Login({ setToken }) {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const token = await loginUser({
+      username,
+      password
+    });
+    setToken(token);
+  }
+
+  return(
+    <div className="login-wrapper">
+      <h1>Please Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Username</p>
+          <input type="text" onChange={e => setUserName(e.target.value)} />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" onChange={e => setPassword(e.target.value)} />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
+};
